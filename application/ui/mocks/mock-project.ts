ee7d0316fb3560374bfd539546653ed7375a7408
Alexander Barabanov <97449232+AlexanderBarabanov@users.noreply.github.com>
2025-12-18 12:23:04 +0000

    Revert "Feature/geti inspect"

--- application/ui/mocks/mock-project.ts
+++
@@ -1,9 +0,0 @@
-import type { SchemaProjectList } from '@geti-inspect/api/spec';
-
-export const getMockedProject = (
-    partial?: Partial<SchemaProjectList['projects'][number]>
-): SchemaProjectList['projects'][number] => ({
-    id: 'project-1',
-    name: 'Test Project',
-    ...partial,
-});
--- application/ui/src/features/inspect/dataset/types.ts
+++
@@ -1,3 +0,0 @@
-import { SchemaMediaList } from '@geti-inspect/api/spec';
-
-export type MediaItem = SchemaMediaList['media'][number];
